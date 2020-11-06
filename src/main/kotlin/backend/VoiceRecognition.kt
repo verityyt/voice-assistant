fd74ed974b297a53b93bc9813361ee5dd8d03e9a
verityyt <joshua-schmidt@online.de>
2020-11-06 21:48:43 +0100

    :construction: Add startActiveRecognition and add sysouts/comments

--- src/main/kotlin/backend/VoiceRecognition.kt
+++ src/main/kotlin/backend/VoiceRecognition.kt
@@ -6,35 +6,44 @@ import java.awt.TextArea
 
 object VoiceRecognition {
 
+    val greetings = listOf("hallo", "guten morgen", "guten tag", "guten abend", "servus", "Hi")
+    val offers = listOf("Wie kann ich ihnen helfen?", "Kann ich irgendetwas für sie tun?", "Kann ich ihnen irgendwie behilflich sein?")
+
     fun startRecognition() {
 
         VoiceAssistant.driver.get("https://translate.google.com/?hl=de&ui=tob&sl=de&tl=en&op=translate")
         val microphoneButton = VoiceAssistant.driver.findElement(By.className("MtoyUd"))
 
+        println("[VoiceRecognition] Starting general recognition for keyword")
         microphoneButton.click() // Starting recognition
 
-        while(true) {
+        var success = false
+
+        while(!success) {
             if(VoiceAssistant.driver.pageSource.toLowerCase().contains("hey zeus")) {
 
+                println("[VoiceRecognition] Recognized keyword, stopping general recognition")
                 microphoneButton.click() // Stopping recognition
 
                 val clearButton = VoiceAssistant.driver.findElement(By.className("GA2I6e"))
                 clearButton.click() // Clearing textarea
 
+                println("[VoiceRecognition] Starting specific recognition")
                 microphoneButton.click() // Restart recognition
                 SoundManager.playSound("start")
 
-                while(true) {
+                while(!success) {
                     val firstDoc = Jsoup.parse(VoiceAssistant.driver.pageSource)
                     val firstElement = firstDoc.getElementsByClass("D5aOJc").first()
 
-                     Thread.sleep(2000)
+                     Thread.sleep(2000) // Checking if user stopped speaking within 2 seconds
 
                     val secondDoc = Jsoup.parse(VoiceAssistant.driver.pageSource)
                     val secondElement = secondDoc.getElementsByClass("D5aOJc").first()
 
                     if(firstElement == secondElement) {
 
+                        println("[VoiceRecognition] Recognized stopped speaking, stopping specific recognition and handling input")
                         microphoneButton.click() // Stop recognition
                         SoundManager.playSound("end")
 
@@ -42,8 +51,8 @@ object VoiceRecognition {
                         val element = doc.getElementsByClass("D5aOJc").first()
 
                         handleRecognition(element.text())
+                        success = true
 
-                        return
                     }
                 }
             }
@@ -51,9 +60,51 @@ object VoiceRecognition {
 
     }
 
+    private fun startActiveRecognition() {
+
+        VoiceAssistant.driver.get("https://translate.google.com/?hl=de&ui=tob&sl=de&tl=en&op=translate")
+        val microphoneButton = VoiceAssistant.driver.findElement(By.className("MtoyUd"))
+
+        println("[VoiceRecognition] Starting new active specific recognition")
+        microphoneButton.click() // Starting recognition
+        SoundManager.playSound("start")
+
+        var success = false
+
+        while(!success) {
+            val firstDoc = Jsoup.parse(VoiceAssistant.driver.pageSource)
+            val firstElement = firstDoc.getElementsByClass("D5aOJc").first()
+
+            Thread.sleep(2000) // Checking if user stopped speaking within 2 seconds
+
+            val secondDoc = Jsoup.parse(VoiceAssistant.driver.pageSource)
+            val secondElement = secondDoc.getElementsByClass("D5aOJc").first()
+
+            if(firstElement == secondElement) {
+
+                println("[VoiceRecognition] Recognized stopped speaking, stopping specific recognition and handling input")
+                microphoneButton.click() // Stop recognition
+                SoundManager.playSound("end")
+
+                val doc = Jsoup.parse(VoiceAssistant.driver.pageSource)
+                val element = doc.getElementsByClass("D5aOJc").first()
+
+                handleRecognition(element.text())
+                success = true
+
+            }
+        }
+
+    }
+
     private fun handleRecognition(textArea: String) {
 
-        println("Du hast '$textArea' gesagt!")
+        if(greetings.contains(textArea.toLowerCase())) {
+            VoiceSynthesizer.speakText("Hallo, ${offers.random()}")
+            startActiveRecognition()
+        }else if(textArea.toLowerCase() == "stopp") {
+            startRecognition()
+        }
 
     }
 
