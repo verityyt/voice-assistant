4ab95df369521abc45eb608a5a326daaabc09f9c
Verity <joshua-schmidt@online.de>
2021-03-15 22:52:02 +0100

    :arrow_up: Convert khttp and selenium to gradle not local dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "jitpack.io"
+        url = uri("https://jitpack.io")
+    }
 }
 
 dependencies {
@@ -17,4 +21,6 @@ dependencies {
     implementation("javax.mail:mail:1.4.1")
     implementation("com.googlecode.json-simple:json-simple:1.1")
     implementation("org.jsoup:jsoup:1.8.3")
+    implementation("com.github.jkcclemens:khttp:-SNAPSHOT")
+    implementation("org.seleniumhq.selenium:selenium-java:3.141.59")
 }
