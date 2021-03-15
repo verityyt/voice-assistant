012f755a2a0ce66d993d3f8a5704d37fb35e032b
Verity <joshua-schmidt@online.de>
2021-03-15 13:24:46 +0100

    :sparkles: Rework StopCommand to fit new system

--- src/main/kotlin/VoiceAssistant.kt
+++ src/main/kotlin/VoiceAssistant.kt
@@ -30,7 +30,8 @@ object VoiceAssistant {
         ImBackCommand(),
         IntroduceCommand(),
         NoCommand(),
-        ShutdownCommand()
+        ShutdownCommand(),
+        StopCommand()
     )
 
     @JvmStatic
--- src/main/kotlin/backend/commands/StopCommand.kt
+++ src/main/kotlin/backend/commands/StopCommand.kt
@@ -1,15 +1,14 @@
 package backend.commands
 
 import backend.core.VoiceCommand
-import backend.core.VoiceRecognition
 
 class StopCommand : VoiceCommand() {
 
+    override val needsReaction: Boolean = false
+
     override val keywords: List<String> = listOf("stopp", "abbruch", "abbrechen")
 
-    override fun perform(input: String) {
-        /*VoiceRecognition.startRecognition()*/
-    }
+    override fun perform(input: String) { }
 
 
 }
