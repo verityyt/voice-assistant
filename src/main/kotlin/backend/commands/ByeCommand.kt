ebf86408db4885aca5b862e9bf1a06a62251c986
Verity <joshua-schmidt@online.de>
2021-03-15 01:03:31 +0100

    :construction: Update ByeCommand to fit new VoiceCommand structure

--- src/main/kotlin/backend/commands/ByeCommand.kt
+++ src/main/kotlin/backend/commands/ByeCommand.kt
@@ -6,11 +6,12 @@ import backend.core.VoiceSynthesizer
 
 class ByeCommand : VoiceCommand() {
 
+    override val needsReaction: Boolean = false
+
     override val keywords: List<String> = listOf("tschüss", "bis später", "bis bald")
 
     override fun perform(input: String) {
         VoiceSynthesizer.speakText("$input. Ich warte solange auf sie")
-        VoiceRecognition.startRecognition()
     }
 
 }
