dda3ed6b44c66094718df9f774ccdb0c42f68f39
Verity <joshua-schmidt@online.de>
2021-03-15 10:55:08 +0100

    :sparkles: Rework Setup to fit new system

---
+++ src/main/kotlin/backend/commands/tasks/SetupTask.kt
@@ -0,0 +1,54 @@
+package backend.commands.tasks
+
+import backend.core.*
+
+class SetupTask : VoiceCommand() {
+
+    override val needsReaction: Boolean = true
+
+    override val keywords: List<String> = listOf()
+
+    private var state = 0
+
+    override fun perform(input: String) {
+        VoiceSynthesizer.speakText("Guten Tag sir, ich starte die Einrichtung. Soll ich neue Einstellungen importieren")
+        SoundManager.playSound("start")
+        state = 1
+    }
+
+    override fun reaction(input: String) {
+        when (state) {
+            1 -> {
+                if (input == "ja") {
+                    VoiceSynthesizer.speakText("Aus welcher Datei soll ich die Einstellungen importieren")
+                    SoundManager.playSound("start")
+                    state = 2
+                } else if (input == "nein") {
+                    VoiceSynthesizer.speakText("Alle Systeme startklar. Ich bin wieder online.")
+                    VoiceRecognizer.currentCommand = null
+                    VoiceRecognizer.activated = false
+                } else {
+                    VoiceSynthesizer.speakText("Ich konnte sie leider nicht verstehen, bitte wiederholen sie ihre Antwort")
+                    SoundManager.playSound("start")
+                }
+            }
+            2 -> {
+                VoiceSynthesizer.speakText("Importiere Einstellungen aus ${input}.json")
+                val import = Configuration.import(input)
+
+                if (import) {
+                    VoiceSynthesizer.speakText("Import erfolgreich, Einstellungen wurden übernommen")
+                    Configuration.setOptionsValue("setup", "false")
+
+                    VoiceSynthesizer.speakText("Alle Systeme startklar. Ich bin wieder online.")
+                    VoiceRecognizer.currentCommand = null
+                    VoiceRecognizer.activated = false
+                } else {
+                    VoiceSynthesizer.speakText("Das importieren aus ${input}.json ist fehlgeschlagen, bitte versuchen sie es mit einer anderen Datei")
+                    SoundManager.playSound("start")
+                }
+            }
+        }
+    }
+
+}
