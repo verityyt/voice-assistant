f518bb835175fde2aca3deba043ea304d382e13d
Verity <joshua-schmidt@online.de>
2021-03-15 01:03:08 +0100

    :sparkles: Add function and varibale to VoiceCommand

--- src/main/kotlin/backend/core/VoiceCommand.kt
+++ src/main/kotlin/backend/core/VoiceCommand.kt
@@ -2,8 +2,12 @@ package backend.core
 
 abstract class VoiceCommand {
 
+    abstract val needsReaction: Boolean
+
     abstract val keywords: List<String>
 
     abstract fun perform(input: String)
+
+    fun reaction(input: String) {}
 
 }
