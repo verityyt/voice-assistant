e30646e18164f8fd6525766b9412b9efbe56fc2f
Verity <joshua-schmidt@online.de>
2021-03-18 18:46:31 +0100

    :construction: Disable all start sounds (+ add explanation)

--- src/main/kotlin/backend/core/SoundManager.kt
+++ src/main/kotlin/backend/core/SoundManager.kt
@@ -8,15 +8,25 @@ import javax.sound.sampled.AudioInputStream
 object SoundManager {
 
     fun playSound(name: String) {
-        val clip = AudioSystem.getClip()
-        val inputStream: AudioInputStream = AudioSystem.getAudioInputStream(File("files/sounds/$name.wav"))
-        clip.open(inputStream)
-        clip.start()
-
-        if(name == "start") {
+        if (name != "start") {
+            val clip = AudioSystem.getClip()
+            val inputStream: AudioInputStream = AudioSystem.getAudioInputStream(File("files/sounds/$name.wav"))
+            clip.open(inputStream)
+            clip.start()
+        } else {
             HUD.play("waiting")
             HUD.show()
         }
+
+        /*
+        *
+        * If you're confused:
+        * This whole thing with those weird if-statements,
+        * is supposed to play not start sound and to show and
+        * play the waiting on start-sounds actually position/time
+        *
+        */
+
     }
 
 }
